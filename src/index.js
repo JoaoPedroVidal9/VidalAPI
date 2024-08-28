aa2750987b9832fb4abb6a12991ddce2e9f44456
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2024-08-28 13:53:58 -0300

    exercicio login

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
-const express = require('express')
+const express = require("express");
 
-class AppController{
+class AppController {
+  constructor() {
+    this.express = express();
+    this.middlewares();
+    this.routes();
+  }
+  middlewares() {
+    this.express.use(express.json());
+  }
+  routes() {
+    const users = [];
 
-    constructor(){
-        this.express = express();
-        this.middlewares();
-        this.routes();
-    }
-    middlewares(){
-        this.express.use(express.json());
-    }
-    routes(){
-        this.express.get('/health/', (req, res) => {res.send({nome:"Evelyn", idade:"17", CPF: 47806760881});
+    this.express.post("/users/", (req, res) => {
+      const { id, nome, email, senha } = req.body;
+      users.push({ id, nome, email, senha });
+      res.status(200).send({ message: "Usuário cadastrado com sucesso" });
     });
 
-    }
+    this.express.get("/health/", (req, res) => {
+      res.status(200).send({ message:"OK" });
+    });
+
+    this.express.get("/users/:id", (req, res) => {
+        const {id} = req.params;
+        const user = users.find((user) => user.id == id);
+        if(user){
+            res.status(200).send(user);
+        }else{
+            res.status(400).send({message: "Usuário não encontrado"});
+        }
+      });
+
+      this.express.post("/auth/", (req, res) => {
+        const { email, senha } = req.body;
+        const user = users.find((user) => user.email == email && user.senha == senha);
+        console.log(user);
+        if(user){
+                res.status(200).send({message: "Authenticado com sucesso!"});
+            }else{
+                res.status(400).send({message: "Algo deu errado"});
+            }
+        });
+  }
 }
 
 module.exports = new AppController().express;
